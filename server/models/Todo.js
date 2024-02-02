c85601f608395ea95750122f01cb802d79a02e07
Abiram <145326881+uthayakumar-Abiram@users.noreply.github.com>
2024-02-02 15:53:09 +0530

    Update Todo.js

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose'); 
 
 //Create schema contains a single field named 'name.' 
-//The 'name' field is of type String
+//The 'name' field is of type String?
+var a =5 
 const TodoSchema = new mongoose.Schema({ name: String }); 
 
 //Export the Mongoose model with the collection name "Todo"
